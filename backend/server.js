95382b5732a9892d13a23ef4c06297281798a885
prakshark <prakhar112358@gmail.com>
2025-07-05 19:28:50 +0530

    Mongoose debugging in deployement

--- backend/server.js
+++ backend/server.js
@@ -47,12 +47,10 @@ const connectDB = async () => {
         }
         
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
             socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
-            bufferMaxEntries: 0, // Disable mongoose buffering
-            bufferCommands: false, // Disable mongoose buffering
+            maxPoolSize: 10, // Maximum number of connections in the pool
+            minPoolSize: 1, // Minimum number of connections in the pool
         });
         
         console.log(`MongoDB Connected: ${conn.connection.host}`);
