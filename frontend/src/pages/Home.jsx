792d7317bf53e1cb4ad419b7ad580bcd70a91bf1
prakshark <prakhar112358@gmail.com>
2025-07-05 20:45:58 +0530

    frontend debugging

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/test/hero'
+import Hero from '../components/test/Hero'
 import { FeaturesSectionDemo } from '../components/ui/BentoGrid'
 
 function Home() {
